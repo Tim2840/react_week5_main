28b8343ccc1adfa6ac5befda02f95b98d682aec9
Tim <gg23456789tw@gmail.com>
2026-02-26 15:57:00 +0800

    feat: Set up initial React application entry point and configure ESLint for React development with hooks and refresh.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./assets/all.scss";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <App />,
-  /* </StrictMode>, */
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
